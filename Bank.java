12de8ce786988319996f691b6e94222f79abe0d3
Papsy <57874831+uxp123@users.noreply.github.com>
2020-08-14 17:55:46 +0000

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package papsyfresh;
+//Developer: Philip Fynn
 
-/**
- *
- * @author philipfynn
- */
 public class Bank {
      private String bankName;
     private String location;
